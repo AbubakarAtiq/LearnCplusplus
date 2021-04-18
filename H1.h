ccd9320050dc3241297ab352e23e356ff38e9ef1
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-04-18 23:00:01 +0500

    init

---
+++ H1.h
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class WelcomePage
+{
+    public:
+    void f1()
+    {
+        cout<<"\tCreated by Abubakar Atiq"<<endl;
+        string a;
+        cout<<"What is your name:";
+        getline(cin,a);
+        cout<<"Welcome,"<<a<<",nice meeting you"<<endl;
+        string a1;
+        cout<<"Do you want to continue this program?(yes or no):";
+        getline(cin,a1);
+        if (a1=="yes") {
+            cout<<"Program started again"<<endl;
+            f1();
+        }
+        else if (a1=="YES")
+        {
+              cout<<"Program started again"<<endl;
+            f1();
+        }
+        else if (a1=="y")
+        {
+              cout<<"Program started again"<<endl;
+            f1();
+        }
+        else if (a1=="Y")
+        {
+              cout<<"Program started again"<<endl;
+            f1();
+            
+        }
+        else if (a1=="NO") {
+        cout<<"Program is terminated,BYE BYE,"<<a<<endl;
+        }
+        else if (a1=="no") {
+        cout<<"Program is terminated,BYE BYE,"<<a<<endl;
+        }
+        else if (a1=="n") {
+        cout<<"Program is terminated,BYE BYE,"<<a<<endl;
+        }
+        else if (a1=="N") {
+        cout<<"Program is terminated,BYE BYE,"<<a<<endl;
+        }
+    }
+
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string>
+#include "H1.h"
+using namespace std;
+int main()
+{
+    cout<<"Hello sir"<<endl;
+    WelcomePage wp;
+    wp.f1();
+    return 0;
+}

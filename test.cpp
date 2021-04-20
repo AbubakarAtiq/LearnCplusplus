468c99252dd3c074d8a758d5b6664903a2161f50
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-04-21 02:12:52 +0500

    setw works for int-not for string?Why

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <iomanip>
+#include<string>
+using namespace std;
+int main()
+{
+    string abubakar_intro("/tc is this");
+    string abubakar_name("/ta is name");
+    int a=3,b=3,c=5;
+    cout<<"As you can see,abubakar_intro is:"<<setw(5)<<abubakar_intro<<endl;
+    cout<<"Name of abubakar is:"<<setw(11)<<abubakar_name<<endl;
+    cout<<"a"<<setw(5)<<a<<endl;
+    cout<<"b"<<setw(5)<<b<<endl;
+    cout<<"c"<<setw(5)<<c<<endl;
+    
+}

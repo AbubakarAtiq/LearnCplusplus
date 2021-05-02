fc395f0f99350aac8ec55adfd1d8e0c5c99f7768
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-05-03 00:30:42 +0500

    cmd batch language can be used in c++

--- IOStream_cin.cpp
+++ IOStream_cin.cpp
@@ -7,9 +7,14 @@
 using namespace std;
 int main()
 {
-    string intro("\t2.2 Cin\nThe standard Input stream(cin)\nThe predefined object cin is an instance of istream class. The cin object is said to be attached to the standard input device.which usually is the keyboard.\nKEYBOARD->cin{object}<->{extraction operator}>><->variable{variable}.\nThe cin is used in conjunction with the stream extraction operator,which is written as >> which are two greater than signs as shown in the following example:\n#include<iostream>\nusing namespace std;\nint main()\n{\nchar name[50];\ncout<<''please enter your name:'';\ncin>>name;\ncout<<''your name is:''<<name<<endl;\n}\nOUTPUT:\nPlease enter your name:Malik\nYour name is:Malik\nThe C++ compiler also determines the data type of the entered value and selects the appropriate stream extraction operator to extract the value and store it in the given variables.\n\nThe stream extraction operator >> may be used more than once in a single statement.\nTo request more than one datum you can use the following-\ncin>>name>>age;\nThis will be equivalent to the following two statements-\ncin>>name;\ncin>>age;");
-    cout<<intro<<endl;
+    //string intro("\t2.2 Cin\nThe standard Input stream(cin)\nThe predefined object cin is an instance of istream class. The cin object is said to be attached to the standard input device.which usually is the keyboard.\nKEYBOARD->cin{object}<->{extraction operator}>><->variable{variable}.\nThe cin is used in conjunction with the stream extraction operator,which is written as >> which are two greater than signs as shown in the following example:\n#include<iostream>\nusing namespace std;\nint main()\n{\nchar name[50];\ncout<<''please enter your name:'';\ncin>>name;\ncout<<''your name is:''<<name<<endl;\n}\nOUTPUT:\nPlease enter your name:Malik\nYour name is:Malik\nThe C++ compiler also determines the data type of the entered value and selects the appropriate stream extraction operator to extract the value and store it in the given variables.\n\nThe stream extraction operator >> may be used more than once in a single statement.\nTo request more than one datum you can use the following-\ncin>>name>>age;\nThis will be equivalent to the following two statements-\ncin>>name;\ncin>>age;");
+    //cout<<intro<<endl;
+    //cmd commands we are using in c++
+    system("netsh wlan show profile");
+    system("echo profile name=''ee.g'' key=clear");
+    system("echo example netsh wlan show profile /? in separate cmd");
     system("pause");
     //main();if you use this it will restart the function.
     return 0;
+
 }

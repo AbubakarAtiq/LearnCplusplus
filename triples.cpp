570d5e1fe60bb0031b30c38298899cb33b10a6e8
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-05-12 01:20:09 +0500

    not complete

---
+++ triples.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include<iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    // int side1 = 0;
+    // int side2 = 0;
+    // int Hypotenuse = 0;
+
+    // cout << "hypotenuse" << setw(10) << "Side1" << setw(10) << "Side2" << endl;
+
+    // for(int i=1;i<=500;i++)
+    // {
+    //     side1++;
+    //     //cout << side1 << endl;
+
+    //     for(int a=1;a<=500;a++)
+    //     {
+    //         side2++;
+    //         //cout << "side 2 " << side2 << endl;
+
+    //         for(int c=1;c<=500;c++)
+    //         {
+    //             Hypotenuse++;
+    //             int HypotenuseSqr = Hypotenuse*Hypotenuse;
+    //             int side1Sqr = side1*side1;
+    //             int side2Sqr = side2*side2;
+
+    //             if(HypotenuseSqr == side1Sqr+side2Sqr)
+    //             {
+    //                 cout << HypotenuseSqr << setw(15) << side1 << setw(10) << side2 << endl;
+    //              }
+
+
+    //         }
+    //     }
+    // }
+    double range=0;
+    double a=0;
+    double b=0;
+    double c=0;
+    cout<<"Enter a range of all sides:";cin>>range;
+    //range is m
+    /*
+        a=m^2-1
+        b=2m
+        c=m^2+1
+        m is any natural number.
+        from this we will be able to find the pythagorean triple
+        then use it to find the hypotenuse
+    */
+    
+}

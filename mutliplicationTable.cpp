eae6bbabb7c6a6c0962c6337800920e798400da5
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-05-13 23:04:56 +0500

    tabular multiplication

--- mutliplicationTable.cpp
+++ mutliplicationTable.cpp
@@ -1,3 +1,24 @@
+/**
+ * @file Tabular Multiplication.cpp
+    @AbubakarAtiq
+ * @brief 
+    Write a C++ program by using a for loop to print a tabular multiplication
+    as given below. The program should ask the user for the size of the table
+    and then perform the table multiplication accordingly. You can use the setw manipulator
+    to adjust the proper setting of numbers in the table.
+ * @version 0.1
+ * @date 2021-05-13
+ * @Help:
+    How i can make program look like this?
+    Please enter the table size:10
+            1   2   3   4   5   6   7   8   9   10
+        +-----------------------------------------
+    1   |   1   2   3   4   5   6   7   8   9   10
+    2   |   2   4   6   8   10  12  14  16  18  20
+    and soon on. 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include<iostream>
 #include<system_error>
 #include<stdio.h>

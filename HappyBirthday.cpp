3ae764e1981d91703d21b768e3be0126eed59403
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-06-07 23:41:20 +0500

    add this in hb repository

--- HappyBirthday.cpp
+++ HappyBirthday.cpp
@@ -1,1 +1,20 @@
-log
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include <system_error>
+//use instead of #include<windows.h>
+using namespace std;
+int main()
+{
+    for(;;)
+    {
+        system("color 02");
+        cout<<"\t\tHappy Birthday:Hasan Bhai"<<endl;
+        system("color 03");
+        system("color 04");
+        system("color 05");
+        system("color 06");
+        system("color 07");
+    }
+    return 0;
+}
---
+++ HappyBirthday1.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include <system_error>
+//use instead of #include<windows.h>
+using namespace std;
+int main()
+{
+    for(;;)
+    {
+        
+        system("color 02");
+        cout<<"\t\tHappy Birthday:Hasan Bhai"<<endl;
+        system("color 03");
+        system("color 04");
+        system("color 05");
+        system("color 06");
+        system("color 07");
+        system("color 08");
+        system("color 09");
+        system("color 10");
+        system("color 11");
+        system("color 12");
+        system("color 13");
+        system("color 14");
+        system("color 15");
+        system("color 16");
+        system("color 17");
+        system("color 18");
+        system("color 19");
+        system("color 20");
+        system("color 21");
+        system("color 22");
+        system("color 23");
+        system("color 24");
+        system("color 25");
+        system("color 26");
+        system("color 27");
+        system("color 28");
+        system("color 29");
+    }
+    return 0;
+}

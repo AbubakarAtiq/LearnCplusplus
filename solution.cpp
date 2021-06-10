58f9625cc5a7c3368c73b715ba2b06cfe5970868
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-06-10 23:28:09 +0500

    nothing special

---
+++ solution.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <ostream>
+#include <stdlib.h>
+#include<string>
+#include<iomanip>
+#include <system_error>
+//use instead of #include<windows.h>
+using namespace std;
+//  std::ostream& bold_on(std::ostream& os)
+//     {
+//         return os<<"\eAbuBakar Atiq";
+//     }
+//     std::ostream& bold_off(std::ostream& os)
+//     {
+//         return os<<"\e[0m";
+//     }
+int main()
+{
+    // std::cout << "\'4\"\5?\\\a4\b7\f8\n4\r7\t4\v7" << std::endl;
+    // // std::cout<<bold_on<<"bold"<<bold_off<<"non-bold"<<endl;
+    // std::cout << "\vofeofhjfiohwf" << std::endl;
+    system("pause");
+   
+    // return 0;
+}

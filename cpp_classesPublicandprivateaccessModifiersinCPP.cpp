08847963e3e5ba9c1409c7b89035e595c414a839
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-08-23 18:50:02 +0500

    Classes, Public and Private access modifiers inCpp

--- cpp_classesPublicandprivateaccessModifiersinCPP.cpp
+++ cpp_classesPublicandprivateaccessModifiersinCPP.cpp
@@ -4,8 +4,34 @@
 #include <system_error>
 //use instead of #include<windows.h>
 using namespace std;
+class Employee
+{
+    private:
+    int a, b, c;
+    public:
+    int d, e;
+    void setData(int a1, int b1, int c1);//Declaration
+    void getData(){
+        cout<<"The value of a is "<<a<<endl;
+        cout<<"The value of b is "<<b<<endl;
+        cout<<"The value of c is "<<c<<endl;
+        cout<<"The value of d is "<<d<<endl;
+        cout<<"The value of e is "<<e<<endl;
+    }
+};
+void Employee::setData(int a1, int b1, int c1){
+    a=a1;
+    b=b1;
+    c=c1;
+}
 int main()
 {
-    
+    Employee abu;
+    //abu.a=123;-->This will throw error as a private
+    abu.d=34;
+    abu.e=89;
+    abu.setData(1,2,3);
+    abu.getData();
+    cout<<"hi"<<endl;
     return 0;
 }

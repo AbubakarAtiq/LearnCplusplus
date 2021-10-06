d99471651537ae50bb9687d2f2bf88bda1a28f43
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-10-06 18:26:40 +0500

    idk about this

---
+++ build/CMakeFiles/LearnCpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnCpp.dir/LearnCpp.cpp.obj"
+  "CMakeFiles/LearnCpp.dir/LearnCpp.cpp.obj.d"
+  "libLearnCpp.a"
+  "libLearnCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

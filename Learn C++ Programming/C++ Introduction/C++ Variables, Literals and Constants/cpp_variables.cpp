d60566a4b37569c34ca3042524075fc8833dedfd
unknown <zetamaass@gmail.com>
2022-03-28 23:10:41 +0500

    New CPP learning resource

---
+++ Learn C++ Programming/C++ Introduction/C++ Variables, Literals and Constants/cpp_variables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ /**
+ * @brief https://www.programiz.com/cpp-programming/variables-literals
+ * This website is used to learn this topic, I also recommended it.
+ * In programming, a variable is a container (storage area) to hold data.
+ * To indicate the storage area, each variable should be given a unique name (identifier), For example,
+ */
+    int age=14;
+    cout<<endl;main();
+}

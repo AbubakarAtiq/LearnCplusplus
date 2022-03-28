b7cefe9cec6ef5d6f89ba57e3fcd2e7626459b29
unknown <zetamaass@gmail.com>
2022-03-28 23:44:12 +0500

    C++ file for intro to variable

--- Learn C++ Programming/C++ Introduction/C++ Variables, Literals and Constants/cpp_variables.cpp
+++ Learn C++ Programming/C++ Introduction/C++ Variables, Literals and Constants/cpp_variables.cpp
@@ -9,5 +9,26 @@ int main()
  * To indicate the storage area, each variable should be given a unique name (identifier), For example,
  */
     int age=14;
+    /**
+     * here, age is a variable of the int data type, and we have assigned an integer value 14 to it.
+     * Note:
+     * The int data type suggests that the variable can only hold integers.
+     * Similarly, we can use the double data type if we have to store decimals and exponential.
+     * We will learn about all the data types in detail in the next tutorial.
+     * The value of a variable can be changed, hence the name variable.
+     */
+    int age=14;//age is 14
+    age=17;//age is
+    /*
+    =====Rules for naming a variable====
+        1.A variable name can only have alphabets, numbers, and the underscore__.
+        2.A variable name cannot begin with a number.
+        3.It is a preferred practice to begin variable names with a lowercase character.For example, name is preferable to Name.
+        4.A variable name cannot be a keyword. For example, int is a keyword that is used to denote integers.
+        5.A variable name can start with an underscore. However, it is not considered a good practice.
+        Note:
+            We should try to give meaningful names to variables. For example, first_name is a better variable name than fn.
+    */
+
     cout<<endl;main();
 }
